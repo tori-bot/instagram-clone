1a08eccaa1b7074b77ed8a0da3418968033349b5
victoria makena <makenavictoria1@gmail.com>
2022-06-05 20:25:40 +0300

    forms: add form request view

--- insta_app/views.py
+++ insta_app/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from .models import Picture
+from .forms import CreatePost
 # Create your views here.
 def home(request):
     message=f'Hello instagram!'
+    pictures=Picture.objects.all()
     context={
         'message':message,
+        'pictures':pictures,
     }
     return render(request,'index.html',context)
+
+def upload_pic(request):
+    upload=CreatePost()
+    if request.method == 'POST':
+        upload=CreatePost(request.POST,request.FILE)
+        if upload.is_valid():
+            upload.save()
+            return redirect('home')
+        else:
+            return HttpResponse('Please fill the form correctly.')
+    else:
+        context={
+            'upload': upload,
+        }
+        return render(request,'upload.html',context)
