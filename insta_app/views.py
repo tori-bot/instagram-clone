72e904ce0c81cf964a46dbcc59fad554d3c7ae9d
victoria makena <makenavictoria1@gmail.com>
2022-06-05 14:55:32 +0300

    home: initiate urls,view and templates

--- insta_app/views.py
+++ insta_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    message=f'Hello instagram!'
+    context={
+        'message':message,
+    }
+    return render(request,'index.html',context)
